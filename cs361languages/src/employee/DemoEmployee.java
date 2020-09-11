0df9b7ef83660afb431c53313fe31065644c9631
unknown <jbonnelycke@gmail.com>
2020-09-11 16:44:09 -0400

    completed hw1

---
+++ cs361languages/src/employee/DemoEmployee.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package employee;
+
+/**
+ * @author Jack Bonnelycke
+ *
+ */
+
+// Resource: https://www.baeldung.com/java-type-casting
+
+// Complete the provided code
+
+public class DemoEmployee {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee("John", "Smith", "123456790");
+
+		PartTimeEmployee pEmp1 = new PartTimeEmployee("Jane", "Smith", "123456777", 17.5);
+
+//		Employee emp2 = new Employee("Don", "Holmes", "123456799");
+
+//		PartTimeEmployee pEmp2 = new PartTimeEmployee("Melissa", "Will", "123456666", 23.5);
+
+		// Upcasting
+		Employee emp3 = pEmp1;
+		System.out.println("emp3");
+		emp3.whoAmI();
+		// TODO To complete
+		// emp3.whoAmI() // prints "PART TIME EMPLOYEE" because it will always run the child/overridden method, and this is a PT Employee
+		// emp3.getHourlyRate(); // returns syntax error because although this is a PT Employee object, it has an Employee reference, so 
+								 // the compiler recognizes that it should have Employee's methods
+
+		// TODO To complete
+		// Add the condition that test that emp3 is an instance of part time employee at
+		// this time
+		if (emp3 instanceof PartTimeEmployee) {
+		System.out.println("emp3 instance of part time employee"); // printed
+		} else {
+		System.out.println("emp3 not instance of part time employee");
+		}
+
+		// Downcasting 1
+		// TODO Uncomment and run the code
+		// PartTimeEmployee pEmp3 = (PartTimeEmployee) emp1;
+		// TODO To complete
+		// This instruction compiles
+		// This instruction returns a ClassCastException at run time
+		// because downcasting sometimes throws an exception due to going from a more general object to a more specific one. 
+
+		// Downcasting 2
+		// Requires the use of intanceof to avoid a run time cast exception
+		// TODO Look at the solution below
+		if (emp3 instanceof PartTimeEmployee) {
+			System.out.println("emp3 instance of part time employee"); // printed
+			PartTimeEmployee pEmp4 = (PartTimeEmployee) emp3; // cast required
+			System.out.println(pEmp4.getHourlyRate());
+		} else {
+			System.out.println("emp3 not instance of part time employee");
+		}
+	}
+}
---
+++ cs361languages/src/mystack/MyStack.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package mystack;
+
+/**
+ * @author Jack Bonnelycke
+ *
+ */
+
+// Stack using generics
+
+// Complete the provided code
+
+public class MyStack<T> {
+
+	private class MyNode<T1> {
+		T1 val;
+		MyNode<T1> next;
+
+		MyNode(T1 v, MyNode<T1> n) {
+			val = v;
+			next = n;
+		}
+	}
+
+	private MyNode<T> theStack = null;
+
+	/**
+	 * 
+	 */
+	public MyStack() {
+	}
+
+	public T pop() {
+		if (theStack != null) {
+			T temp = theStack.val;
+			theStack = theStack.next;
+			return temp;
+		}
+		else return null;
+	}
+
+	public void push(T v) {
+		theStack = new MyNode<T>(v, theStack);
+	}
+	
+	public T peek() {
+		return theStack.val;
+	}
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO To complete
+		// Create a stack of Integer
+		MyStack<Integer> intStack = new MyStack<Integer>();
+		// Push 1 and 2
+		intStack.push(1);
+		intStack.push(2);
+		// Pop
+		intStack.pop();
+		// Push 5
+		intStack.push(5);
+		
+		// TODO To complete
+		// Create a stack of Person
+		MyStack<Person> personStack = new MyStack<Person>();
+		// Push a person p1 with your name
+		Person p1 = new Person("Jack", "Bonnelycke");
+		personStack.push(p1);
+		// Push a person p2 with my name
+		Person p2 = new Person("Christelle", "Scharff");
+		personStack.push(p2);
+	}
+
+}
